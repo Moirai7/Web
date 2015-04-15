4925f60bd7be6dfeccccb2d9042d05a81521d2f2
Emma <emma@moirai.cn>
2015-04-15 21:06:17 +0800

    update admin

---
+++ data/conf/config.php
@@ -0,0 +1,7 @@
+<?php	return array (
+  'SP_MAIL_ADDRESS' => '12301055@bjtu.edu.cn',
+  'SP_MAIL_SENDER' => '张岚',
+  'SP_MAIL_SMTP' => 'smtp.bjtu.edu.cn',
+  'SP_MAIL_LOGINNAME' => '12301055@bjtu.edu.cn',
+  'SP_MAIL_PASSWORD' => '742612',
+);?>
---
+++ data/runtime/Cache/User/fd1212954ae5b6be1c5937ae9024261a.php
@@ -0,0 +1,273 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+<head>
+<title><?php echo ($site_name); ?></title>
+<meta name="keywords" content="<?php echo ($site_seo_keywords); ?>" />
+<meta name="description" content="<?php echo ($site_seo_description); ?>">
+<meta name="author" content="ThinkCMF">
+	<?php $portal_index_lastnews=2; $portal_hot_articles="1,2"; $portal_last_post="1,2"; $tmpl=sp_get_theme_path(); $default_home_slides=array( array( "slide_name"=>"ThinkCMFX1.6.0发布啦！", "slide_pic"=>$tmpl."Public/images/demo/1.jpg", "slide_url"=>"", ), array( "slide_name"=>"ThinkCMFX1.6.0发布啦！", "slide_pic"=>$tmpl."Public/images/demo/2.jpg", "slide_url"=>"", ), array( "slide_name"=>"ThinkCMFX1.6.0发布啦！", "slide_pic"=>$tmpl."Public/images/demo/3.jpg", "slide_url"=>"", ), ); ?>
+	<meta name="author" content="ThinkCMF">
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    
+    <!-- Set render engine for 360 browser -->
+    <meta name="renderer" content="webkit">
+
+   	<!-- No Baidu Siteapp-->
+    <meta http-equiv="Cache-Control" content="no-siteapp"/>
+
+    <!-- HTML5 shim for IE8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <![endif]-->
+	<link rel="icon" href="/TYT/tpl/simplebootx/Public/images/favicon.ico" type="image/x-icon">
+	<link rel="shortcut icon" href="/TYT/tpl/simplebootx/Public/images/favicon.ico" type="image/x-icon">
+    <link href="/TYT/tpl/simplebootx/Public/simpleboot/themes/cmf/theme.min.css" rel="stylesheet">
+    <link href="/TYT/tpl/simplebootx/Public/simpleboot/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+    <link href="/TYT/tpl/simplebootx/Public/simpleboot/font-awesome/4.2.0/css/font-awesome.min.css"  rel="stylesheet" type="text/css">
+	<!--[if IE 7]>
+	<link rel="stylesheet" href="/TYT/tpl/simplebootx/Public/simpleboot/font-awesome/4.2.0/css/font-awesome-ie7.min.css">
+	<![endif]-->
+	<link href="/TYT/tpl/simplebootx/Public/css/style.css" rel="stylesheet">
+	<style>
+		/*html{filter:progid:DXImageTransform.Microsoft.BasicImage(grayscale=1);-webkit-filter: grayscale(1);}*/
+		#backtotop{position: fixed;bottom: 50px;right:20px;display: none;cursor: pointer;font-size: 50px;z-index: 9999;}
+		#backtotop:hover{color:#333}
+		#main-menu-user li.user{display: none}
+	</style>
+	
+<style>
+.control-label{
+	font-weight: bold;
+	float: left;
+	width: 70px;
+}
+</style>
+</head>
+<body class="body-white" id="top">
+	<?php echo hook('body_start');?>
+<div class="navbar navbar-fixed-top">
+   <div class="navbar-inner">
+     <div class="container">
+       <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+         <span class="icon-bar"></span>
+         <span class="icon-bar"></span>
+         <span class="icon-bar"></span>
+       </a>
+       <a class="brand" href="/TYT/"><img src="/TYT/tpl/simplebootx/Public/images/logo.png"/></a>
+       <div class="nav-collapse collapse" id="main-menu">
+       	<?php
+ $effected_id=""; $filetpl="<a href='\$href' target='\$target'>\$label</a>"; $foldertpl="<a href='\$href' target='\$target' class='dropdown-toggle' data-toggle='dropdown'>\$label <b class='caret'></b></a>"; $ul_class="dropdown-menu" ; $li_class="" ; $style="nav"; $showlevel=6; $dropdown='dropdown'; echo sp_get_menu("main",$effected_id,$filetpl,$foldertpl,$ul_class,$li_class,$style,$showlevel,$dropdown); ?>
+		
+		<ul class="nav pull-right" id="main-menu-user">
+			<li class="dropdown user login">
+	            <a class="dropdown-toggle user" data-toggle="dropdown" href="#">
+	            <img src="/TYT/tpl/simplebootx//Public/images/headicon.png" class="headicon"/>
+	            <span class="user-nicename"></span><b class="caret"></b></a>
+	            <ul class="dropdown-menu pull-right">
+	               <li><a href="<?php echo u('user/center/index');?>"><i class="fa fa-user"></i> &nbsp;个人中心</a></li>
+				   <?php
+ if(get_admin_type()){ ?>
+				   <li><a href="/TYT/admin"><i class="fa fa-user"></i> &nbsp;后台管理</a></li>
+				   <?php
+ } ?>
+	               <li class="divider"></li>
+	               <li><a href="<?php echo u('user/index/logout');?>"><i class="fa fa-sign-out"></i> &nbsp;退出</a></li>
+	            </ul>
+          	</li>
+          	<li class="dropdown user offline">
+	            <a class="dropdown-toggle user" data-toggle="dropdown" href="#">
+	           		<img src="/TYT/tpl/simplebootx//Public/images/headicon.png" class="headicon"/>登录<b class="caret"></b>
+	            </a>
+	            <ul class="dropdown-menu pull-right">
+	               <li><a href="<?php echo U('api/oauth/login',array('type'=>'sina'));?>"><i class="fa fa-weibo"></i> &nbsp;微博登录</a></li>
+	               <li><a href="<?php echo U('api/oauth/login',array('type'=>'qq'));?>"><i class="fa fa-qq"></i> &nbsp;QQ登录</a></li>
+	               <li><a href="<?php echo u('user/login/index');?>"><i class="fa fa-sign-in"></i> &nbsp;登录</a></li>
+	               <li class="divider"></li>
+	               <li><a href="<?php echo u('user/register/index');?>"><i class="fa fa-user"></i> &nbsp;注册</a></li>
+	            </ul>
+          	</li>
+		</ul>
+		<div class="pull-right">
+        	<form method="post" class="form-inline" action="<?php echo U('portal/search/index');?>" style="margin:18px 0;">
+				 <input type="text" class="" placeholder="Search" name="keyword" value="<?php echo I('get.keyword');?>"/>
+				 <input type="submit" class="btn btn-info" value="Go" style="margin:0"/>
+			</form>
+		</div>
+       </div>
+     </div>
+   </div>
+ </div>
+
+		<div class="container tc-main">
+                <div class="row">
+                    <div class="span3">
+	                    <div class="list-group">
+	<a class="list-group-item" href="<?php echo u('user/profile/edit');?>"><i class="fa fa-list-alt"></i> 修改资料</a>
+	<a class="list-group-item" href="<?php echo u('user/profile/password');?>"><i class="fa fa-lock"></i> 修改密码</a>
+	<a class="list-group-item" href="<?php echo u('user/profile/avatar');?>"><i class="fa fa-user"></i> 编辑头像</a>
+	<a class="list-group-item" href="<?php echo u('user/profile/bang');?>"><i class="fa fa-exchange"></i> 绑定账号</a>
+	<a class="list-group-item" href="<?php echo u('user/favorite/index');?>"><i class="fa fa-star-o"></i> 我的收藏</a>
+	<a class="list-group-item" href="<?php echo u('comment/comment/index');?>"><i class="fa fa-comments-o"></i> 我的评论</a>
+</div>
+                    </div>
+                    <div class="span9">
+                           <div class="tabs">
+                               <ul class="nav nav-tabs">
+                                   <li class="active"><a href="#one" data-toggle="tab"><i class="fa fa-list-alt"></i>个人中心</a></li>
+                               </ul>
+                               <div class="tab-content">
+                                   <div class="tab-pane active" id="one">
+                                   		  <div class="span2">
+                                   		  <a href="<?php echo U('profile/avatar');?>">
+                                   			<?php if(empty($avatar)): ?><img src="/TYT/tpl/simplebootx//Public/images/headicon_128.png" class="headicon"/>
+								            <?php else: ?>
+								            <img src="<?php echo sp_get_user_avatar_url($avatar);?>" class="headicon"/><?php endif; ?>
+								          </a>
+										  </div>      
+										  <div class="span3">
+                                   			<div class="control-group">
+                                   				<label class="control-label" for="input-user_nicename">昵称</label>
+                                   				<div class="controls">
+                                   					<?php echo ((isset($user_nicename) && ($user_nicename !== ""))?($user_nicename):'未填写'); ?>
+                                   				</div>
+                                   			</div>
+                                   			<div class="control-group">
+                                   				<label class="control-label" for="input-sex">性别</label>
+                                   				<div class="controls">
+                                   				<?php $sexs=array("0"=>"保密","1"=>"程序猿","2"=>"程序媛"); ?>
+                                   					<?php echo ($sexs[$sex]); ?>
+                                   				</div>
+                                   			</div>
+                                   			<div class="control-group">
+                                   				<label class="control-label" for="input-birthday">生日</label>
+                                   				<div class="controls">
+                                   					<?php echo ((isset($birthday) && ($birthday !== ""))?($birthday):'未填写'); ?>
+                                   				</div>
+                                   			</div>
+                                   			<div class="control-group">
+                                   				<label class="control-label" for="input-user_url">个人网址</label>
+                                   				<div class="controls">
+                                   					<?php echo ((isset($user_url) && ($user_url !== ""))?($user_url):'未填写'); ?>
+                                   				</div>
+                                   			</div>
+                                   			<div class="control-group">
+                                   				<label class="control-label" for="input-signature">个性签名</label>
+                                   				<div class="controls">
+                                   					<?php echo ((isset($signature) && ($signature !== ""))?($signature):'未填写'); ?>
+                                   				</div>
+                                   			</div>
+                                   			<div class="control-group">
+                                   				<div class="controls">
+                                   					<a href="<?php echo U('user/profile/edit');?>" type="submit" class="btn">编辑</a>
+                                   				</div>
+                                   			</div>
+                                   		</div>
+                                   </div>
+                               </div>							
+                           </div>
+                    </div>
+                </div>
+
+		<br><br><br>
+<!-- Footer
+      ================================================== -->
+      <hr>
+<?php echo hook('footer');?>
+      <div id="footer">
+        <div class="links">
+        <?php $links=sp_getlinks(); ?>
+        <?php if(is_array($links)): foreach($links as $key=>$vo): ?><a href="<?php echo ($vo["link_url"]); ?>" target="<?php echo ($vo["link_target"]); ?>"><?php echo ($vo["link_name"]); ?></a><?php endforeach; endif; ?>
+        </div>
+        <p>
+        Made by <a href="http://www.thinkcmf.com">ThinkCMF</a>
+        Code licensed under the <a href="http://www.apache.org/licenses/LICENSE-2.0" rel="nofollow" target="_blank">Apache License v2.0</a>.<br/>
+        Based on <a href="http://getbootstrap.com/2.3.2/" target="_blank">Bootstrap</a>.  Icons from <a href="http://fortawesome.github.com/Font-Awesome/" target="_blank">Font Awesome</a>
+        </p>
+      </div>
+      <div id="backtotop"><i class="fa fa-arrow-circle-up"></i></div>
+      <?php echo ($site_tongji); ?>
+
+
+	</div>
+	<!-- /container -->
+
+	<script type="text/javascript">
+//全局变量
+var GV = {
+    DIMAUB: "/TYT/",
+    JS_ROOT: "statics/js/",
+    TOKEN: ""
+};
+</script>
+<!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="/TYT/statics/js/jquery.js"></script>
+    <script src="/TYT/statics/js/wind.js"></script>
+    <script src="/TYT/tpl/simplebootx/Public/simpleboot/bootstrap/js/bootstrap.min.js"></script>
+    <script src="/TYT/statics/js/frontend.js"></script>
+	<script>
+	$(function(){
+		$('body').on('touchstart.dropdown', '.dropdown-menu', function (e) { e.stopPropagation(); });
+		
+		$("#main-menu li.dropdown").hover(function(){
+			$(this).addClass("open");
+		},function(){
+			$(this).removeClass("open");
+		});
+		
+		$.post("<?php echo U('user/index/is_login');?>",{},function(data){
+			if(data.status==1){
+				if(data.user.avatar){
+					$("#main-menu-user .headicon").attr("src",data.user.avatar.indexOf("http")==0?data.user.avatar:"/TYT/data/upload/avatar/"+data.user.avatar);
+				}
+				
+				$("#main-menu-user .user-nicename").text(data.user.user_nicename!=""?data.user.user_nicename:data.user.user_login);
+				$("#main-menu-user li.user.login").show();
+				
+			}
+			if(data.status==0){
+				$("#main-menu-user li.user.offline").show();
+			}
+			
+		});	
+		;(function($){
+			$.fn.totop=function(opt){
+				var scrolling=false;
+				return this.each(function(){
+					var $this=$(this);
+					$(window).scroll(function(){
+						if(!scrolling){
+							var sd=$(window).scrollTop();
+							if(sd>100){
+								$this.fadeIn();
+							}else{
+								$this.fadeOut();
+							}
+						}
+					});
+					
+					$this.click(function(){
+						scrolling=true;
+						$('html, body').animate({
+							scrollTop : 0
+						}, 500,function(){
+							scrolling=false;
+							$this.fadeOut();
+						});
+					});
+				});
+			};
+		})(jQuery); 
+		
+		$("#backtotop").totop();
+		
+		
+	});
+	</script>
+
+
+</body>
+</html>
