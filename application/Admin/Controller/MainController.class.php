92b29af9bc41a2086ce25df2074232a08b59525c
Emma <emma@moirai.cn>
2015-04-17 17:42:37 +0800

    update

--- application/Admin/Controller/MainController.class.php
+++ application/Admin/Controller/MainController.class.php
@@ -15,7 +15,7 @@ class MainController extends AdminbaseController {
     			'运行环境' => $_SERVER["SERVER_SOFTWARE"],
     			'PHP运行方式' => php_sapi_name(),
     			'MYSQL版本' =>$mysql,
-    			'程序版本' => SIMPLEWIND_CMF_VERSION . "&nbsp;&nbsp;&nbsp; [<a href='http://www.thinkcmf.com' target='_blank'>访问官网</a>]",
+    			'程序版本' => SIMPLEWIND_CMF_VERSION . "&nbsp;&nbsp;&nbsp; ",
     			'上传附件限制' => ini_get('upload_max_filesize'),
     			'执行时间限制' => ini_get('max_execution_time') . "秒",
     			'剩余空间' => round((@disk_free_space(".") / (1024 * 1024)), 2) . 'M',
