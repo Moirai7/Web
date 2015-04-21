85df61e22e28c8362bcb5f8d03567320f2da1a15
Yao Jie <12301111@bjtu.edu.cn>
2015-04-21 09:01:59 +0800

    前台内容
    从github同步下来的文件我这边看不到效果啊，不知道是哪儿出了问题，我就先把这个代码传上去，样式还没改

--- application/Portal/Controller/IndexController.class.php
+++ application/Portal/Controller/IndexController.class.php
@@ -8,6 +8,19 @@ class IndexController extends HomeBaseController {
 	
     //首页
 	public function index() {
+		$links_obj= M("Links");
+		$father = $links_obj->where("link_status=1")->order("listorder ASC")->where("link_type=0")->select();
+		//var_dump($father);
+		$falength = count($father);
+		$sons = array();
+		foreach ($father as $key=>$value) {
+			 $sons[$key] = $links_obj->where("link_status=1")->order("listorder ASC")->where("link_type=%d",$value['link_id'])->select();	
+			}
+		//var_dump($father);
+		//var_dump($sons);
+		$this->assign("falength",$falength);
+		$this->assign("father",$father);
+		$this->assign("sons",$sons);
     	$this->display(":index");
     }   
 
